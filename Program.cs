f3451ebec9d4884dd914860428ce64a24d26a6c5
Vadim <suokasvrn@gmail.com>
2022-08-04 21:20:08 +0300

    array output method

--- Program.cs
+++ Program.cs
@@ -25,4 +25,19 @@ string[] ThreeCharacters(string[] array, int size)
    return Arr;
 }
 
+void ShowStringArray(string[] array)
+{
+   for(int i =0; i < array.Length; i++)
+   {
+      Console.Write(array[i] + " ");
+   }
+
+   Console.WriteLine();
+}
+
+
 string[] myString = {"hello", "2", "world", ":-)"}; //{"1234", "1567", "-2", "computer science"}; {"Russia", "Denmark", "Kazan"};
+int size = NumberOfLines(myString);
+
+string[] newString = ThreeCharacters(myString, size);
+ShowStringArray(newString);
