b00809b6e6ed4bfeac17fcb3a17399d813a55382
Vadim <suokasvrn@gmail.com>
2022-08-04 21:15:28 +0300

    method to create a new array of strings less than three characters

--- Program.cs
+++ Program.cs
@@ -11,4 +11,18 @@
    return size;
 }
 
+string[] ThreeCharacters(string[] array, int size)
+{
+   string[] Arr = new string[size];
+
+   for(int i =0; i < array.Length; i++)
+         if(array[i].Length < 4)
+         {
+         Arr[Arr.Length - size] = array[i];
+         size--;
+         }
+   
+   return Arr;
+}
+
 string[] myString = {"hello", "2", "world", ":-)"}; //{"1234", "1567", "-2", "computer science"}; {"Russia", "Denmark", "Kazan"};
